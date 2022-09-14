f6741c2b5c3f9eed732bb8cdeb89b349f924eb7e
teferasamuel <teferasamuel67@gmail.com>
2022-09-14 11:04:10 -0700

    Fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,8 @@ class Square:
     """Class Square that defines a square object
     """
     def __init__(self, size):
-    """Initialize method that stores the size of the square
-    Args:
-    param1 (int): size of the square
-    """
-    self.__size = size
+        """Initialize method that stores the size of the square
+        Args:
+        param1 (int): size of the square
+        """
+        self.__size = size
