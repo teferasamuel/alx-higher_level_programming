396b7fb6a9fbb3d5c1d0a1790fafbc4cebb60f9c
teferasamuel <teferasamuel67@gmail.com>
2022-09-28 08:55:57 -0700

    fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,6 +5,7 @@ representation of an object
 """
 import json
 
+
 def to_json_string(my_obj):
     """ Function that returns the JSON representation of an object
     Args:
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Module that contains a function that returns an object by
 a JSON representation
 """
