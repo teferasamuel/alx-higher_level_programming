bd189c412b458fae1ec127a5ea9645c87704b3ae
teferasamuel <teferasamuel67@gmail.com>
2022-09-29 05:16:59 -0700

    it works

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """ Empty class """
+
+    def area(self):
+        """area def"""
+        raise Exception("area() is not implemented")
