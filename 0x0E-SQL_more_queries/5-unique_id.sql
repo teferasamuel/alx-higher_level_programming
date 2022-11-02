38fba3c8387678f443181e716df2b14fb90a4360
teferasamuel <teferasamuel67@gmail.com>
2022-11-02 11:01:31 -0700

    fix

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
-cript that creates a table
+-- Script that creates a table
 -- Query to create the table 'unique_id' in MySQL server
 CREATE TABLE IF NOT EXISTS unique_id (
 	       id INT UNIQUE DEFAULT 1,
